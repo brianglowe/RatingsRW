cfddc69eb39995c203af2d3fc3350e9369ad3f27
Brian Glowe <brianglowe@gmail.com>
2015-11-10 11:57:56 -0500

    updated the PlayersVC contorller to fullfill the custom tableview cell

--- Ratings/Ratings/PlayersViewController.swift
+++ Ratings/Ratings/PlayersViewController.swift
@@ -37,16 +37,40 @@ class PlayersViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return players.count
     }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)
-        -> UITableViewCell {
-            let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath)
-            
-            let player = players[indexPath.row] as Player
-            cell.textLabel?.text = player.name
-            cell.detailTextLabel?.text = player.game
-            return cell
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath)
+        //1_(lines above) dequeueReusableCellWithIdentifier will dequeue an existing cell with the reuse identifier PlayerCell if available or create a new one if not.
+        
+        let player = players[indexPath.row] as Player
+        //2_(line above) you look up the Player object correspoinding to the row being populated and assing it to player
+        
+        if let nameLabel = cell.viewWithTag(100) as? UILabel {
+        //3(above) it is key to use the custom TB cell tag to assign values. the labels and images are looked up by their tag on the cell and populated with data from the player object.
+            nameLabel.text = player.name
+        }
+        if let gameLabel = cell.viewWithTag(101) as? UILabel {
+            gameLabel.text = player.game
+        }
+        if let ratingImageView = cell.viewWithTag(102) as? UIImageView {
+            ratingImageView.image = self.imageForRating(player.rating)
+        }
+        return cell
     }
+    /*
+    the code below was used before a custom table view cell was created
+    it shows the changes to cellForRowAtIndexPath required to used custom TB cell
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)
+    -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath)
+    
+    let player = players[indexPath.row] as Player
+    cell.textLabel?.text = player.name
+    cell.detailTextLabel?.text = player.game
+    return cell
+    }
+    */
 
     func imageForRating(rating:Int) -> UIImage? {
         let imageName = "\(rating)Stars"
